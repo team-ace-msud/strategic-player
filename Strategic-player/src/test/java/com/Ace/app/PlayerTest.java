bcbfb5aa234f031c0eab2818567745d4d961808c
Emenu Dobamo <birhanu.emenu@gmail.com>
2018-10-06 00:59:25 -0600

    add a test class for player.java

---
+++ Strategic-player/src/test/java/com/Ace/app/PlayerTest.java
@@ -0,0 +1,15 @@
+package com.Ace.app;
+
+import junit.framework.TestCase;
+
+public class PlayerTest extends TestCase {
+
+    public void testBeginGame() {
+    }
+
+    public void testGetSlotsToReveal() {
+    }
+
+    public void testGetNewCoinStates() {
+    }
+}
