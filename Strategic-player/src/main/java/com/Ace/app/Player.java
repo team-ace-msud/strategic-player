fe44084fe0286b811d3dabb4b30357bdd3ea4ef7
Emenu Dobamo <birhanu.emenu@gmail.com>
2018-10-06 08:03:49 -0600

    implement getSlotsToReveal method

--- Strategic-player/src/main/java/com/Ace/app/Player.java
+++ Strategic-player/src/main/java/com/Ace/app/Player.java
@@ -2,16 +2,28 @@ package com.Ace.app;
 
 public class Player implements StrategicPlayer {
 
+    CharSequence coinState;
+    int coinsPerWheel;
+    int revealsPerSpin;
+
+
 
     @Override
     public void beginGame(int coinsPerWheel, int revealsPerSpin, int maxNumSpins) {
         System.out.println("Welcome to the Game.  ");
+
+
+
     }
 
     @Override
-    public CharSequence getSlotsToReveal(CharSequence blankPattern)
-    {
-        return null;
+    public CharSequence getSlotsToReveal(CharSequence blankPattern){
+
+    coinState = new StringBuilder();
+        for(int i = 0; i < blankPattern.length(); i++) {
+        ((StringBuilder) coinState).append(" -");
+    }
+            return coinState;
     }
 
     @Override
