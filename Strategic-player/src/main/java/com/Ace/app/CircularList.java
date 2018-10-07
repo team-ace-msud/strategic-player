d2599d4b6d8986c2bdb962d169c76fcd07cfa2b7
emenuDobamo <birhanu.emenu@gmail.com>
2018-10-06 22:40:38 -0600

    Update CircularList.java

--- Strategic-player/src/main/java/com/Ace/app/CircularList.java
+++ Strategic-player/src/main/java/com/Ace/app/CircularList.java
@@ -1,5 +1,5 @@
 package com.Ace.app;
-/*Copyright 2018 Principal Software Engineers
+/*Copyright 2018 ACE
         Permission is hereby granted, free of charge, to any person obtaining a copy of this software
         and associated documentation files (the "Software"), to deal in the Software without restriction,
         including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
