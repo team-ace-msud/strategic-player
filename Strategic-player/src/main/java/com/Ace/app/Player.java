fe06541e637c6876e4cee6511ef1286db06c2474
Emenu Dobamo <birhanu.emenu@gmail.com>
2018-10-08 10:11:01 -0600

    add getSlotsToREveal method

--- Strategic-player/src/main/java/com/Ace/app/Player.java
+++ Strategic-player/src/main/java/com/Ace/app/Player.java
@@ -1,28 +1,42 @@
 package com.Ace.app;
 
+import java.util.Random;
+
 public class Player implements StrategicPlayer {
 
-    CharSequence coinState;
-    int coinsPerWheel;
-    int revealsPerSpin;
-
+    private CharSequence coinState;
+    private int coinsPerWheel;
+    private int revealsPerSpin;
+    private int maxNumSpins;
 
 
     @Override
     public void beginGame(int coinsPerWheel, int revealsPerSpin, int maxNumSpins) {
         System.out.println("Welcome to the Game.  ");
+        this.coinsPerWheel = coinsPerWheel;
+        this.revealsPerSpin = revealsPerSpin;
+        this.maxNumSpins = maxNumSpins;
 
 
 
     }
 
     @Override
-    public CharSequence getSlotsToReveal(CharSequence blankPattern){
-
+    public CharSequence getSlotsToReveal(){
+    Random random = new Random();
     coinState = new StringBuilder();
-        for(int i = 0; i < blankPattern.length(); i++) {
-        ((StringBuilder) coinState).append(" -");
-    }
+        for(int i = 0; i < coinsPerWheel; i++) {
+            if(random.nextBoolean() == true) {
+                ((StringBuilder) coinState).append(" -");
+            }
+            else if(revealsPerSpin > 0) {
+                ((StringBuilder) coinState).append(" ?");
+                revealsPerSpin--;
+            }
+
+
+        }
+
             return coinState;
     }
 
