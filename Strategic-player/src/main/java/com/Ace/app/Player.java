b36790778f2b0024f3f9c8bc5723f2e43569c35d
Emenu Dobamo <birhanu.emenu@gmail.com>
2018-10-11 11:14:37 -0600

    update the interface

--- Strategic-player/src/main/java/com/Ace/app/Player.java
+++ Strategic-player/src/main/java/com/Ace/app/Player.java
@@ -1,6 +1,7 @@
 package com.Ace.app;
 
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Player implements StrategicPlayer {
 
@@ -8,11 +9,13 @@ public class Player implements StrategicPlayer {
     private int coinsPerWheel;
     private int revealsPerSpin;
     private int maxNumSpins;
+    private Random random = new Random();
+
 
 
     @Override
     public void beginGame(int coinsPerWheel, int revealsPerSpin, int maxNumSpins) {
-        System.out.println("Welcome to the Game.  ");
+        //System.out.println("Welcome to the Game.  ");
         this.coinsPerWheel = coinsPerWheel;
         this.revealsPerSpin = revealsPerSpin;
         this.maxNumSpins = maxNumSpins;
@@ -23,39 +26,14 @@ public class Player implements StrategicPlayer {
 
     @Override
     public CharSequence getSlotsToReveal(){
-    Random random = new Random();
-    coinState = new StringBuilder();
-        for(int i = 0; i < coinsPerWheel; i++) {
-            if(revealsPerSpin > 0) {
-                ((StringBuilder) coinState).append("?");
-                revealsPerSpin--;
 
-            }
-            else {
-                ((StringBuilder) coinState).append("-");
-            }
-
-
-        }
 
             return coinState;
     }
 
     @Override
     public CharSequence getNewCoinStates(CharSequence revealedPattern) {
-        CharSequence newCharSequence = new StringBuilder();
-        System.out.println(revealedPattern);
 
-        for (int i = 0; i < 4; i ++){
-            if(revealedPattern.charAt(i) == '?'){
-                ((StringBuilder) newCharSequence).append('H');
-            }
-            else {
-                ((StringBuilder) newCharSequence).append('-');
-
-            }
-        }
-        System.out.println(newCharSequence);
 
         return null;
     }
